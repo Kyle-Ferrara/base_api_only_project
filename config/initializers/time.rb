f1af833c91832d9bb3915ac309181e683a3c0266
Kyle Ferrara <kyleferrara@offerzen.com>
2022-10-13 18:35:02 +0200

    [Sequel] setup timestamps

--- config/initializers/time.rb
+++
@@ -1,9 +0,0 @@
-# Time::DATE_FORMATS[:db] = '%Y-%m-%d %H:%M:%S.%9N'
-
-# module ActiveSupport
-#   class TimeWithZone
-#     def as_json(_options = {})
-#       strftime('%Y-%m-%dT%H:%M:%S.%6N%z')
-#     end
-#   end
-# end
